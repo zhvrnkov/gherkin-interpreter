905533a34debf6a28e3afcaec9a7af263e870365
zhvrnkov <v.zhvrnkv@gmail.com>
2020-09-27 20:14:15 +0600

    Evaling first step of test scenario

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-scenario = """Given user is-on start screen
+scenario = """Given user is on start-screen
 And user is not authorized
 When user taps profile button
 Then user goes to profile screen
@@ -6,17 +6,34 @@ Then user goes to profile screen
 
 Exp = (str, list)
 
+keywords = ["on", "is", "Given"]
+
 class Env(dict):
 
     @classmethod
     def default(cls):
         return cls({
-            "is-on": lambda where: where(),
-            "is": lambda state: state()  
+            "on": lambda where: where,
+            "is": lambda state: state,
+            "start-screen": "start screen",
+            "Given": Env.given_action,
+            "user": None
         })
+
+    @staticmethod
+    def given_action(subject, envID):
+        global current_env
+        current_env = Env(Env.dictforid(envID), current_env)
+
+    @staticmethod
+    def dictforid(id: str) -> dict:
+        return {
+            "start screen": Env.startscreen_dict(),
+            "profile screen": Env.profilescreen_dict()
+        }[id]
     
     @staticmethod
-    def startScreenDict() -> dict:
+    def startscreen_dict() -> dict:
         return {
             # "authorized": lambda: bool(random.getrandbits(1)), # authorization checker
             "profile button": "profile_button_element_id",
@@ -24,7 +41,7 @@ class Env(dict):
         }
 
     @staticmethod
-    def profileScreenDict(outerEnv = None) -> dict:
+    def profilescreen_dict() -> dict:
         return {
             "avatar": "avatar_element_id",
             "logout button": "logout_button_id"
@@ -33,11 +50,8 @@ class Env(dict):
     def __init__(self, dict, outer=None):
         self.update(dict)
         self.outer = outer
-
-currentEnv = Env.default()
-
-gherkinKeywords = ["Given", "user", "When", "Then"]
-keywords = ["is-on", "is", "not", "taps", "goes"]
+        
+current_env = Env.default()
 
 def parse(step: str) -> list:
     return read_from_tokens(tokenize(step))
@@ -46,16 +60,29 @@ def tokenize(step: str) -> list:
     return step.split()
 
 def read_from_tokens(tokens: list) -> Exp:
-    exp = []
-    l = exp
+    if len(tokens) == 1:
+        return str(tokens[0])
+    exps = []
+    l = exps
     for token in tokens:
-        if token in keywords:
-            _l = [token]
-            l = _l
-            exp.append(_l)
-        else:
+        if token not in keywords:
             l.append(token)
-    return exp
+        elif len(l) == 0:
+            l.append(token)
+        else:
+            l.append([token])
+            l = l[-1]
+    return exps
     
-def eval(exp: Exp, env: Env):
-    pass
+def eval(exp: Exp):
+    env = current_env
+    if isinstance(exp, str):
+        return env[exp]
+    else:
+        op, *args = exp
+        proc = eval(op)
+        args = [eval(arg) for arg in args]
+        print(args)
+        return proc(*args)
+
+    
